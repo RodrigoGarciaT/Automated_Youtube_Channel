524354938b289820c7ece0d045ebb4e26b8f5f82
RodrigoGarciaT <rodrigo.garcia.torres04@gmail.com>
2022-08-15 23:30:02 -0500

    Finished the first web scraping function.
    Got the links of the posts inside the subreddit, by using both css and xpath formats.

--- RedditScraper.py
+++ RedditScraper.py
@@ -3,27 +3,32 @@ from scrapy.crawler import CrawlerProcess
 
 
 class RedditSpider(scrapy.Spider):
-    name = "reddit_spider"
+    name = "RedditSpider"
 
     def start_requests(self):
-
         urls = [
             'https://www.reddit.com/r/AmItheAsshole/hot/',
-             'https://www.reddit.com/r/confessions/',
-             'https://www.reddit.com/r/tifu/'
+             'https://www.reddit.com/r/confessions/hot/',
+             'https://www.reddit.com/r/tifu/hot/'
                ]
         for url in urls:
-            yield scrapy.request(url=url, callback=self.get_post_links)
+            yield scrapy.Request(url=url, callback=self.get_post_links)
 
     def get_post_links(self, response):
-        pass
-        # web scraping the url of every single
 
+        # web scraping the url of every single post
+        urls = response.css('div._1poyrkZ7g36PawDueRza-J > div._1ixsU4oQRnNfZ91jhBU74y > div._3-miAEojrCvx_4FQ8x3P-s').xpath('./a/@href').extract()
         # Sending every post to be web scraped
-        urls = []
+
         for url in urls:
+            print(url)
             yield response.follow(url=url, callback=self.get_post_information)
 
     def get_post_information(self, response):
         pass
         #web scraping each individual post
+
+
+process = CrawlerProcess()
+process.crawl(RedditSpider)
+process.start()
