5b7a60f2f36af9a22544d14718acb6c88e2dc637
RodrigoGarciaT <rodrigo.garcia.torres04@gmail.com>
2022-08-16 20:59:00 -0500

    Extracted a list of titles and text from every post and got an engine to read them.

--- RedditScraper.py
+++ RedditScraper.py
@@ -6,7 +6,7 @@ voice = engine.getProperty('voices')
 final_links = []
 final_titles = []
 final_text = []
-
+story = 0
 
 class RedditSpider(scrapy.Spider):
     name = "RedditSpider"
@@ -25,29 +25,25 @@ class RedditSpider(scrapy.Spider):
         # web scraping the url of every single post
         urls = response.css('div._1poyrkZ7g36PawDueRza-J > div._1ixsU4oQRnNfZ91jhBU74y > div._3-miAEojrCvx_4FQ8x3P-s').xpath('./a/@href').extract()
         # Sending every post to be web scraped
-
         for url in urls:
             print(url)
             yield response.follow(url=url, callback=self.get_post_information)
 
     def get_post_information(self, response):
+        global story
         #web scraping each individual post
         titles = response.css('h1._eYtD2XCVieq6emjKBH3m::text').extract()
         final_links.append(response.url)
         final_titles.append(titles[0])
-        #print(titles[0])
-        text = response.css('div._292iotee39Lmt0MkQZ2hPV ::text').extract()
-        final_text.append(text[0])
-        #print(text[0])
-        print(text)
+        text = response.xpath('//div[@data-test-id="post-content"]').css('div._292iotee39Lmt0MkQZ2hPV ::text').extract()
+        final_text.append(text)
+
 
 process = CrawlerProcess()
 process.crawl(RedditSpider)
 process.start()
 
-print(final_titles[0])
-print(final_text[0])
 engine.setProperty('voice', voice[2].id)
-engine.say(final_titles[0])
-engine.say(final_text[0])
+engine.say(final_titles[7])
+engine.say(final_text[7])
 engine.runAndWait()
